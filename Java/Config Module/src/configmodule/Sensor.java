ff1b628753e16e38fe29ad69cd36352b4cdd09f8
Rizwan Ahmad <rizwanahmad93@gmail.com>
2014-02-27 14:06:52 -0700

    Finished sensor class

--- Java/Config Module/src/configmodule/Sensor.java
+++ Java/Config Module/src/configmodule/Sensor.java
@@ -1,5 +1,5 @@
 package configmodule;
-import javax.comm.*;
+import com.phidgets.*;
 
 // TODO depending on the commonalities between the sensors, we can either make a polymorphic structure of sensors, make all separate classes for each
 // 		type and ignore a parent class, or include them all in this class.  I don't care, but I dont know enough at this time to choose for us
@@ -8,48 +8,57 @@ public class Sensor implements Runnable {
 	
 	private int orientationDegree; // where is the sensor facing? 0 = right, 90 = front...
 	private sensorType type;
+	private InterfaceKitPhidget phidget;
+	private int threshold;
+	private int port;
 	
 	private static final double sonar2cm = 1.296;
 	
-	private int[] sensorData = new int[20]; // keeps track of the last 20 previous polls of sensor data in the event we need to reference to make
-											// an informed decision about the surrounding environment
+	private double lastValue;
 			
 			
-	Sensor(sensorType type, int orientationDegree)
+	Sensor(sensorType type, int orientationDegree, int serial, int threshold, int port) throws PhidgetException
 	{
 		this.type = type;
 		this.orientationDegree = orientationDegree;
+		this.port = port;
+		this.phidget = new InterfaceKitPhidget();
+		this.phidget.open(serial);
+		phidget.waitForAttachment();
 	}
 	
 	/*****************************************************
 	 * Description:	Polls the sensor for its value
 	 * 
 	 * Returns:	The distance in ____ to nearest object // TODO choose distance measurement unit
+	 * @throws PhidgetException 
 	 ****************************************************/
-	public int pollDistance()
+	public double pollSensor() throws PhidgetException
 	{
 		if(this.type == sensorType.sonar) // sonar sensor
 		{
+			double distance = phidget.getSensorValue(port)*sonar2cm;
+			if(distance < threshold)
+				return -1;
+			
+			lastValue = distance;
+			return distance;
 			
 		}
-		else // reflective sensor
+		else if(this.type == sensorType.reflective) // reflective sensor
 		{
 			
 		}
+		else // compass sensor
+		{
+			
+		}
+		
+		return 0;
 	}
 	
-	/*****************************************************
-	 * Description:	Continuously polls the sensor for data
-	 ****************************************************/
-	public void run()
+	public void addToArray()
 	{
-		while (true)
-		{
-			for(int i = 0; i < 20; i++)
-			{	
-				this.sensorData[i] = pollDistance(); // using array due to the quick nature of the data type vs a linked list that takes lot of time to traverse
-				Thread.sleep(100); // sleep thread for 100ms between data polling
-			}
-		}
+		
 	}
 }
